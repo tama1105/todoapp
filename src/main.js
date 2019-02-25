6a6f8aeae3a52bcd117f2fa30e8f0df597722f56
tomato5515 <important1105@outlook.jp>
2019-02-25 15:41:17 +0900

    introduced react

--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
 const {app, BrowserWindow} = require('electron');
-
+const Datastore = require('nedb');
+let db = new Datastore({ filename: './todo.db' });
 let win;
+// db.loadDatabase(function (err) {    // Callback is optional
+//   // Now commands will be executed
+// });
+// TODO: using react
+
 function createWindow() {
   //ウィンドウを作成
   win = new BrowserWindow({width: 800, height: 600});
 
   //index.htmlを表示
-  win.loadFile('index.html')
+  win.loadFile(__dirname + '/renderer/index.html');
 
   //開発者ツールを開く
   win.webContents.openDevTools();

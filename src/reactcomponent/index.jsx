40b4f16e56b22e69d8ff5f5b4bf58357895a15fd
tomato5515 <important1105@outlook.jp>
2019-02-28 15:37:35 +0900

    introduce materialui

--- src/reactcomponent/index.jsx
+++ src/reactcomponent/index.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
 import {render} from 'react-dom';
+import Button from '@material-ui/core/Button';
 
 class App extends React.Component {
   render () {
-    return <div>a</div>;
+    return (
+    <Button variant="contained" color="primary">
+      Hello World
+    </Button>
+  );
   }
+}
+
+class TaskList extends React.Component {
+  render(
+    
+  )
 }
 
 render(<App/>, document.getElementById('app'));
